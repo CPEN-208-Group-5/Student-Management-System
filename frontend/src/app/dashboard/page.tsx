8e7787843ea16ef5309ceb1ff3f3b99aaec08d92
George <geogyamfi36@gmail.com>
2025-08-04 13:47:24 +0000

    updated dashboard ui

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import Fees from "@/components/Fees";
 
 export default function DashboardPage() {
   return (
-    <div className="text-black  bg-white p-6 min-h-screen">
+    <div className="text-black mt-4 bg-white p-6 min-h-screen">
       <h1 className="text-3xl font-extrabold text-blue-800 tracking-tight">Student Dashboard</h1>
       <Profile />
       <div className="grid md:grid-cols-2 gap-4 grid-cols-1">
